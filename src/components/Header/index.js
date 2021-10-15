d94cf77e4caf59bb768b731283eb5924f6737a6a
ttavaresdev <ttavares.develop@gmail.com>
2021-10-14 21:46:04 -0400

    Primeiro Commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {Feather} from '@expo/vector-icons';
+
+import {Container, MenuButton, Title} from './styles';
+import{NavigationContainer, useNavigation} from '@react-navigation/native';
+
+function Header ({title}){
+  const navigation = useNavigation();
+  return(
+  <Container>
+
+    <MenuButton onPress={() => navigation.openDrawer()}>
+     <Feather name="menu" size={36} color="#fff"/>
+    </MenuButton>
+
+    <Title>{title}</Title>
+  </Container>
+  )
+}
+
+export default Header;
---
+++ src/page/Movies/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import{ View, Text} from 'react-native';
+
+function Movies (){
+  return(
+    <View>
+      <Text> Meus filmes </Text>
+    </View>
+  )
+}
+export default Movies;
